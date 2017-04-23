bd45f2cae90b689725101290686e4f80f9ad7a52
Kandai Watanabe <kandai.wata@gmail.com>
2017-04-23 16:27:22 +0900

    cansatクラス追加

---
+++ cansat.cpp
@@ -0,0 +1,21 @@
+/**
+* @file cansat.cpp
+* @author Kandai Watanabe
+* @date Created: 20170420
+* @date Modified: 20170420
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include "Arduino.h"
+#include "cansat.h"
+
+Cansat::Cansat(){
+}
+
+Cansat::~Cansat(){
+  clear();
+}
+
+void Cansat::clear(){
+}
---
+++ cansat.h
@@ -0,0 +1,21 @@
+/**
+* @file cansat.h
+* @author Kandai Watanabe
+* @date Created: 20170423
+* @date Modified: 2017043
+*/
+#ifndef _CANSAT_H_
+#define _CANSAT_H_
+// #include <vector>
+// #include "~~.h"
+
+class Cansat {
+public:
+  Cansat();
+  ~Cansat();
+
+private:
+  void clear();
+};
+
+#endif
