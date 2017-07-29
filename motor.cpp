eb733765b65583dccdbf12c84faf6de587c0076b
Kandai Watanabe <kandai.wata@gmail.com>
2017-07-29 21:19:47 +0900

    モータに上下限追加

--- motor.cpp
+++ motor.cpp
@@ -20,18 +20,20 @@ Motor::~Motor(){
 
 void Motor::setSpeedAt(int v){
   // 正転・逆転どっちか
+  if(v<0) v=0;
+  if(v>255) v=0;
   digitalWrite(_pinMotorFin, HIGH);
   analogWrite(_pinMotorRin, v);
 }
 
 void Motor::stop(){
   breaking();
- digitalWrite(_pinMotorFin, LOW);
- digitalWrite(_pinMotorRin, LOW);
+  digitalWrite(_pinMotorFin, LOW);
+  digitalWrite(_pinMotorRin, LOW);
 }
 
 void Motor::breaking(){
- digitalWrite(_pinMotorFin, HIGH);
- digitalWrite(_pinMotorRin, HIGH);
- delay(100);
+  digitalWrite(_pinMotorFin, HIGH);
+  digitalWrite(_pinMotorRin, HIGH);
+  delay(100);
 }
